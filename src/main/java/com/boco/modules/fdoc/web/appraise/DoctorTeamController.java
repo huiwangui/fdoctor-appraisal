e35e1e9066ca84ba775e1804499ac0885a7985ec
Listsummer <503749530@qq.com>
2017-08-10 16:53:21 +0800

    分数模型字段修改为基础类型

--- src/main/java/com/boco/modules/fdoc/web/appraise/DoctorTeamController.java
+++ src/main/java/com/boco/modules/fdoc/web/appraise/DoctorTeamController.java
@@ -21,116 +21,90 @@ import com.boco.modules.fdoc.model.score.HospitalEntity;
 import com.boco.modules.fdoc.service.score.AppraisalMonthOrgScoreService;
 import com.boco.modules.fdoc.service.score.AppraisalMonthTeamScoreService;
 import com.boco.modules.fdoc.vo.AppraisalMonthTeamScoreVo;
- 
- 
- 
- 
 
 @Controller
-@RequestMapping(value="/doctorTeam",produces = "application/json;charset=UTF-8")
+@RequestMapping(value = "/doctorTeam", produces = "application/json;charset=UTF-8")
 public class DoctorTeamController {
-	
+
 	@Resource
 	AppraisalMonthOrgScoreService appraisalMonthOrgScoreService;
 	@Resource
 	AppraisalMonthTeamScoreService appraisalMonthTeamScoreService;
-	
-	@RequestMapping(value = "/homePage",method = RequestMethod.GET)
-	public String showPage(HttpServletRequest request, Model model){
-		//获取所有医疗机构
-		List<HospitalEntity> organizationList = appraisalMonthOrgScoreService.getHospitalList();		 
-		//设置作用域对象
+
+	@RequestMapping(value = "/homePage", method = RequestMethod.GET)
+	public String showPage(HttpServletRequest request, Model model) {
+		// 获取所有医疗机构
+		List<HospitalEntity> organizationList = appraisalMonthOrgScoreService
+				.getHospitalList();
+		// 设置作用域对象
 		model.addAttribute("organizationList", organizationList);
 		return "/appraise/doctorTeam/homePage";
 	}
-	
+
 	@ResponseBody
-	@RequestMapping(value="teamScoreList",method=RequestMethod.GET)
-	public String teamScoreList(HttpServletRequest request, Model model,String orgId,String  month,String  doctorName,
-				Integer pageIndex,Integer pageSize) throws ParseException{
+	@RequestMapping(value = "teamScoreList", method = RequestMethod.GET)
+	public String teamScoreList(HttpServletRequest request, Model model,
+			String orgId, String month, String doctorName, Integer pageIndex,
+			Integer pageSize) throws ParseException {
 		AppraisalMonthTeamScoreVo vo = new AppraisalMonthTeamScoreVo();
-		Page<AppraisalMonthTeamScoreVo> page=new Page<AppraisalMonthTeamScoreVo>(pageIndex, pageSize);
-		vo.setOrgId(orgId);	 
-		
-		if(StringUtils.isNotEmpty(month)){
+		Page<AppraisalMonthTeamScoreVo> page = new Page<AppraisalMonthTeamScoreVo>(
+				pageIndex, pageSize);
+		vo.setOrgId(orgId);
+
+		if (StringUtils.isNotEmpty(month)) {
 			vo.setMonth(StringUtils.getStringNum(month));
 		}
 		vo.setDoctorName(doctorName);
-		//获取所有医疗团队总数
+		// 获取所有医疗团队总数
 		int count = appraisalMonthTeamScoreService.getTeamScoreListCount(vo);
 		vo.setPage(page);
-		//获取所有医疗团队
-		List<AppraisalMonthTeamScoreVo> teamScoreList = appraisalMonthTeamScoreService.getTeamScoreList(vo);
+		// 获取所有医疗团队
+		List<AppraisalMonthTeamScoreVo> teamScoreList = appraisalMonthTeamScoreService
+				.getTeamScoreList(vo);
 		for (AppraisalMonthTeamScoreVo team : teamScoreList) {
-			team.setDoctorName(team.getDoctorName()+"团队");
-			int familyIncrement = 0;
-			if(team.getFamilyIncrement()!=null){
-				 familyIncrement = team.getFamilyIncrement();
-			}
-			int signIncrement = 0;
-			if(team.getSignIncrement()!=null){
-				signIncrement = team.getSignIncrement();
-			}			 
-			team.setSignFamilyAndPerson(familyIncrement+"/"+signIncrement);
-			if(team.getSignManageScore()==null){
-				team.setSignManageScore(0.0);
-			}
-			if(team.getPublicHealthScore()==null){
-				team.setPublicHealthScore(0.0);
-			}
-			if(team.getHealthManageScore()==null){
-				team.setHealthManageScore(0.0);
-			}
-			if(team.getCustomerScore()==null){
-				team.setCustomerScore(0.0);
-			}
-			if(team.getResultScore()==null){
-				team.setResultScore(0.0);
-			}
-			 
-			 
+			team.setDoctorName(team.getDoctorName() + "团队");
+			int familyIncrement = team.getFamilyIncrement();
+			int signIncrement = team.getSignIncrement();
+			team.setSignFamilyAndPerson(familyIncrement + "/" + signIncrement);
+			team.setSignManageScore(0.0);
+			team.setPublicHealthScore(0.0);
+			team.setHealthManageScore(0.0);
+			team.setCustomerScore(0.0);
+			team.setResultScore(0.0);
+
 		}
-		//return JsonUtils.getJson(teamScoreList);
+		// return JsonUtils.getJson(teamScoreList);
 		return JsonUtils.getJson(BaseJsonVo.pageList(teamScoreList, count));
 	}
-	@RequestMapping(value = "/teamStrength",method = RequestMethod.GET)
-	public String getTeamStrengthNumber(HttpServletRequest request, Model model,String month,String teamId){
+
+	@RequestMapping(value = "/teamStrength", method = RequestMethod.GET)
+	public String getTeamStrengthNumber(HttpServletRequest request,
+			Model model, String month, String teamId) {
 		AppraisalMonthTeamScoreEntity entity = new AppraisalMonthTeamScoreEntity();
 		entity.setMonth(month);
 		entity.setTeamId(teamId);
-		//获取指定月份的指定团队数据
-		AppraisalMonthTeamScoreVo vo = appraisalMonthTeamScoreService.getAppraisalMonthTeamScoreByMonth(entity);
-		//获取指定团队的实力
-		AppraisalMonthTeamScoreVo scoreVo =appraisalMonthTeamScoreService.getAppraisalMonthTeamScoreByTeamId(teamId);
-		if(scoreVo==null){
-			//设置作用域对象
-			model.addAttribute("team", new AppraisalMonthTeamScoreVo()); 
+		// 获取指定月份的指定团队数据
+		AppraisalMonthTeamScoreVo vo = appraisalMonthTeamScoreService
+				.getAppraisalMonthTeamScoreByMonth(entity);
+		// 获取指定团队的实力
+		AppraisalMonthTeamScoreVo scoreVo = appraisalMonthTeamScoreService
+				.getAppraisalMonthTeamScoreByTeamId(teamId);
+		if (scoreVo == null) {
+			// 设置作用域对象
+			model.addAttribute("team", new AppraisalMonthTeamScoreVo());
 			return "/appraise/doctorTeam/teamStrength";
 		}
-		if(vo.getResultScore()==null){
-			scoreVo.setResultScore(0.0);
-		}else{
-			scoreVo.setResultScore(vo.getResultScore());
-		}if(vo.getSignIncrement()==null){
-			scoreVo.setSignIncrement(0);
-		}else{
-			scoreVo.setSignIncrement(vo.getSignIncrement());
-		}if(vo.getFamilyIncrement()==null){
-			scoreVo.setFamilyIncrement(0);
-		}else{
-			scoreVo.setFamilyIncrement(vo.getFamilyIncrement());
-		}if(vo.getChronicDiseaseNumber()==null){
-			scoreVo.setChronicDiseaseNumber(0);
-		}else{
-			scoreVo.setChronicDiseaseNumber(vo.getChronicDiseaseNumber());
-		}
+		scoreVo.setResultScore(vo.getResultScore());
+		scoreVo.setSignIncrement(vo.getSignIncrement());
+		scoreVo.setFamilyIncrement(vo.getFamilyIncrement());
+		scoreVo.setChronicDiseaseNumber(vo.getChronicDiseaseNumber());
 		scoreVo.setOrgAddress(vo.getOrgAddress());
-	    scoreVo.setOrgName(vo.getOrgName());
+		scoreVo.setOrgName(vo.getOrgName());
 		scoreVo.setDoctorName(vo.getDoctorName());
-	 
-		//设置作用域对象
+
+		// 设置作用域对象
 		model.addAttribute("team", scoreVo);
- 
+
 		return "/appraise/doctorTeam/teamStrength";
 	}
 }
--- src/main/java/com/boco/modules/fdoc/web/appraise/OrganizationController.java
+++ src/main/java/com/boco/modules/fdoc/web/appraise/OrganizationController.java
@@ -17,146 +17,97 @@ import com.boco.common.persistence.Page;
 import com.boco.common.utils.JsonUtils;
 import com.boco.common.utils.StringUtils;
 import com.boco.modules.fdoc.model.score.AppraisalMonthOrgScoreEntity;
-import com.boco.modules.fdoc.model.score.AppraisalMonthTeamScoreEntity;
 import com.boco.modules.fdoc.model.score.HospitalEntity;
 import com.boco.modules.fdoc.service.score.AppraisalMonthOrgScoreService;
 import com.boco.modules.fdoc.service.score.AppraisalMonthTeamScoreService;
 import com.boco.modules.fdoc.vo.AppraisalMonthOrgScoreVo;
-import com.boco.modules.fdoc.vo.AppraisalMonthTeamScoreVo;
- 
- 
- 
- 
 
 @Controller
-@RequestMapping(value="/organization",produces = "application/json;charset=UTF-8")
+@RequestMapping(value = "/organization", produces = "application/json;charset=UTF-8")
 public class OrganizationController {
-	
+
 	@Resource
 	AppraisalMonthOrgScoreService appraisalMonthOrgScoreService;
 	@Resource
 	AppraisalMonthTeamScoreService appraisalMonthTeamScoreService;
-	
-	@RequestMapping(value = "/homePage",method = RequestMethod.GET)
-	public String showPage(HttpServletRequest request, Model model){
-		//获取所有医疗机构
-		List<HospitalEntity> organizationList = appraisalMonthOrgScoreService.getHospitalList();		 
-		//设置作用域对象
+
+	@RequestMapping(value = "/homePage", method = RequestMethod.GET)
+	public String showPage(HttpServletRequest request, Model model) {
+		// 获取所有医疗机构
+		List<HospitalEntity> organizationList = appraisalMonthOrgScoreService
+				.getHospitalList();
+		// 设置作用域对象
 		model.addAttribute("organizationList", organizationList);
 		return "/appraise/organization/homePage";
 	}
-	
+
 	@ResponseBody
-	@RequestMapping(value="organizationScoreList",method=RequestMethod.GET)
-	public String teamScoreList(HttpServletRequest request, Model model,String orgId,String  month, 
-				Integer pageIndex,Integer pageSize) throws ParseException{
+	@RequestMapping(value = "organizationScoreList", method = RequestMethod.GET)
+	public String teamScoreList(HttpServletRequest request, Model model,
+			String orgId, String month, Integer pageIndex, Integer pageSize)
+			throws ParseException {
 		AppraisalMonthOrgScoreVo vo = new AppraisalMonthOrgScoreVo();
-		Page<AppraisalMonthOrgScoreVo> page=new Page<AppraisalMonthOrgScoreVo>(pageIndex, pageSize);
-		vo.setOrgId(orgId);	 
-		
-		if(StringUtils.isNotEmpty(month)){
+		Page<AppraisalMonthOrgScoreVo> page = new Page<AppraisalMonthOrgScoreVo>(
+				pageIndex, pageSize);
+		vo.setOrgId(orgId);
+
+		if (StringUtils.isNotEmpty(month)) {
 			vo.setMonth(StringUtils.getStringNum(month));
-		} 
-		//根据月份获取所有医疗机构总数		
-		int count = appraisalMonthOrgScoreService.getOrganizationScoreListCount(vo);
-		vo.setPage(page);
-		//根据月份获取所有医疗机构
-		List<AppraisalMonthOrgScoreVo> organizationScoreList = appraisalMonthOrgScoreService.getOrganizationScoreList(vo);
-		for (AppraisalMonthOrgScoreVo organization : organizationScoreList){		 
-			int familyIncrement = 0;
-			if(organization.getFamilyIncrement()!=null){
-				 familyIncrement = organization.getFamilyIncrement();
-			}
-			int signIncrement = 0;
-			if(organization.getSignIncrement()!=null){
-				signIncrement = organization.getSignIncrement();
-			}			 
-			organization.setSignFamilyAndPerson(familyIncrement+"/"+signIncrement);
-			if(organization.getSignManageScore()==null){
-				organization.setSignManageScore(0.0);
-			}
-			if(organization.getPublicHealthScore()==null){
-				organization.setPublicHealthScore(0.0);
-			}
-			if(organization.getHealthManageScore()==null){
-				organization.setHealthManageScore(0.0);
-			}
-			if(organization.getCustomerScore()==null){
-				organization.setCustomerScore(0.0);
-			}
-			if(organization.getResultScore()==null){
-				organization.setResultScore(0.0);
-			}
-			 
-			 
 		}
-		//return JsonUtils.getJson(teamScoreList);
-		return JsonUtils.getJson(BaseJsonVo.pageList(organizationScoreList,count));
+		// 根据月份获取所有医疗机构总数
+		int count = appraisalMonthOrgScoreService
+				.getOrganizationScoreListCount(vo);
+		vo.setPage(page);
+		// 根据月份获取所有医疗机构
+		List<AppraisalMonthOrgScoreVo> organizationScoreList = appraisalMonthOrgScoreService
+				.getOrganizationScoreList(vo);
+		for (AppraisalMonthOrgScoreVo organization : organizationScoreList) {
+			int familyIncrement = organization.getFamilyIncrement();
+			int signIncrement = organization.getSignIncrement();
+			organization.setSignFamilyAndPerson(familyIncrement + "/"
+					+ signIncrement);
+
+		}
+		// return JsonUtils.getJson(teamScoreList);
+		return JsonUtils.getJson(BaseJsonVo.pageList(organizationScoreList,
+				count));
 	}
-	@RequestMapping(value = "/organizationStrength",method = RequestMethod.GET)
-	public String getTeamStrengthNumber(HttpServletRequest request, Model model,String month,String orgId){
-	  
+
+	@RequestMapping(value = "/organizationStrength", method = RequestMethod.GET)
+	public String getTeamStrengthNumber(HttpServletRequest request,
+			Model model, String month, String orgId) {
+
 		AppraisalMonthOrgScoreEntity entity = new AppraisalMonthOrgScoreEntity();
 		entity.setMonth(month);
 		entity.setOrgId(orgId);
-		//获取指定月份的指定机构数据
-		AppraisalMonthOrgScoreVo vo = appraisalMonthOrgScoreService.getAppraisalMonthOrgScoreByMonth(entity);
-		//获取指定机构的实力
-		AppraisalMonthOrgScoreVo scoreVo =appraisalMonthOrgScoreService.getAppraisalMonthOrgScoreByOrgId(orgId);
-		//处理一个机构名称对应多个orgid 比如仁和区卫生局
-		if(scoreVo==null){
-			//设置作用域对象
-			model.addAttribute("organization", new AppraisalMonthOrgScoreVo());	 
+		// 获取指定月份的指定机构数据
+		AppraisalMonthOrgScoreVo vo = appraisalMonthOrgScoreService
+				.getAppraisalMonthOrgScoreByMonth(entity);
+		// 获取指定机构的实力
+		AppraisalMonthOrgScoreVo scoreVo = appraisalMonthOrgScoreService
+				.getAppraisalMonthOrgScoreByOrgId(orgId);
+		// 处理一个机构名称对应多个orgid 比如仁和区卫生局
+		if (scoreVo == null) {
+			// 设置作用域对象
+			model.addAttribute("organization", new AppraisalMonthOrgScoreVo());
 			return "/appraise/organization/organizationStrength";
 		}
-		
-		if(StringUtils.isNotEmpty(vo.getOrgAddress())){
+
+		if (StringUtils.isNotEmpty(vo.getOrgAddress())) {
 			scoreVo.setOrgAddress(vo.getOrgAddress());
 		}
-		
-		if(vo.getFamilyIncrement()==null){
-			scoreVo.setFamilyIncrement(0);
-		}else{
-			scoreVo.setFamilyIncrement(vo.getFamilyIncrement());
-		}
-		if(vo.getSignIncrement()==null){
-			scoreVo.setSignIncrement(0);
-		}else{
-			scoreVo.setSignIncrement(vo.getSignIncrement());
-		}
-		if(vo.getTeamTotal()==null){
-			scoreVo.setTeamTotal(0);
-		}else{
-			scoreVo.setTeamTotal(vo.getTeamTotal());
-		}
-		if(vo.getChronicDiseaseNumber()==null){
-			scoreVo.setChronicDiseaseNumber(0);
-		}else{
-			scoreVo.setChronicDiseaseNumber(vo.getChronicDiseaseNumber());
-		}	
-		if(vo.getUnQualifiedIncrement()==null){
-			scoreVo.setUnQualifiedIncrement(0);
-		}else{
-			scoreVo.setUnQualifiedIncrement(vo.getUnQualifiedIncrement());
-		}	
-		if(vo.getExcellentIncrement()==null){
-			scoreVo.setExcellentIncrement(0);
-		}else{
-			scoreVo.setExcellentIncrement(vo.getExcellentIncrement());
-		}	
-		if(vo.getQualifiedIncrement()==null){
-			scoreVo.setQualifiedIncrement(0);
-		}else{
-			scoreVo.setQualifiedIncrement(vo.getQualifiedIncrement());
-		}	
-		if(vo.getResultScore()==null){
-			scoreVo.setResultScore(0.0);
-		}else{
-			scoreVo.setResultScore(vo.getResultScore());
-		}	
-		//设置作用域对象
+
+		scoreVo.setFamilyIncrement(vo.getFamilyIncrement());
+		scoreVo.setSignIncrement(vo.getSignIncrement());
+		scoreVo.setTeamTotal(vo.getTeamTotal());
+		scoreVo.setChronicDiseaseNumber(vo.getChronicDiseaseNumber());
+		scoreVo.setUnQualifiedIncrement(vo.getUnQualifiedIncrement());
+		scoreVo.setExcellentIncrement(vo.getExcellentIncrement());
+		scoreVo.setQualifiedIncrement(vo.getQualifiedIncrement());
+		scoreVo.setResultScore(vo.getResultScore());
+		// 设置作用域对象
 		model.addAttribute("organization", scoreVo);
- 
+
 		return "/appraise/organization/organizationStrength";
 	}
 }
